cc28998b8833405a406169fa84485e5d6484827e
Caroline O'Donnell <caroline.odonnell26@gmail.com>
2017-07-25 11:21:12 -0600

    adding production connection

--- knexfile.js
+++ knexfile.js
@@ -5,5 +5,9 @@ module.exports = {
   development: {
     client: 'pg',
     connection: 'postgres://localhost/practice'
-  }
+  },
+  production: {
+  client: 'pg',
+  connection: process.env.DATABASE_URL
+}
 };
